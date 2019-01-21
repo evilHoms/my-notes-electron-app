f32763a32a158a1c09ccd40068b5720749d5676d
evilHoms <homspage@gmail.com>
2019-01-22 01:01:24 +0300

    Gitignore fixes

--- installers/windows/MyNotes.bat
+++
@@ -1,1 +0,0 @@
-wscript.exe D:\development\desktop\electron\my-notes-electron-app\installers\windows\invisible.vbs D:\development\desktop\electron\my-notes-electron-app\installers\windows\windows-start.bat 
--- installers/windows/windows-start.bat
+++
@@ -1,2 +0,0 @@
-::Script to start my-notes app 
-npm run start --prefix D:\development\desktop\electron\my-notes-electron-app\installers\windows\..\.. 
